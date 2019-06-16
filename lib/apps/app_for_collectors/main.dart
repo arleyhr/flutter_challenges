1c3f9ce05185ad80f95679f32f0fe87c5636dc21
Arley HR <arley.hr@hotmail.com>
2019-06-16 12:31:06 -0500

    theme, tabs, console model

--- lib/apps/app_for_collectors/main.dart
+++ lib/apps/app_for_collectors/main.dart
@@ -1,30 +1,60 @@
 import 'package:flutter/material.dart';
 
 
-class AppForCollectors extends StatelessWidget {
+class AppForCollectors extends StatefulWidget {
   const AppForCollectors({Key key}) : super(key: key);
+
+  @override
+  _AppForCollectorsState createState() => _AppForCollectorsState();
+}
+
+class _AppForCollectorsState extends State<AppForCollectors> with SingleTickerProviderStateMixin {
+  TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = new TabController(vsync: this, initialIndex: 0, length: 3);
+  }
 
   @override
   Widget build(BuildContext context) {
     return  MaterialApp(
       title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.red,
+        primaryColor: Color.fromRGBO(63, 68, 157, 1),
+        accentColor: Color.fromRGBO(128, 240, 180, 1),
+        tabBarTheme: TabBarTheme(
+          labelColor: Color.fromRGBO(128, 240, 180, 1),
+          unselectedLabelColor: Colors.white
+        ),
       ),
       home: Scaffold(
         appBar: AppBar(
-          title: Text('collectors'),
-        ),
-        body: Container(
-          child: Center(
-            child: FlatButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed('/');
-              },
-              child: Text('App for collectors'),
-            ),
+          leading: Icon(Icons.search),
+          title: Text('Your collection', style: TextStyle(fontWeight: FontWeight.bold),),
+          centerTitle: true,
+          actions: <Widget>[
+            Icon(Icons.more_vert)
+          ],
+          bottom: TabBar(
+            controller: _tabController,
+            tabs: <Widget>[
+              Tab(text: 'Games'),
+              Tab(text: 'Books'),
+              Tab(text: 'Movies'),
+            ],
           ),
         ),
+        body: TabBarView(
+          controller: _tabController,
+          children: <Widget>[
+            Container(),
+            Container(),
+            Container(),
+          ],
+        )
       ),
     );
   }
