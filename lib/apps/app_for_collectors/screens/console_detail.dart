fd82ef3b2c0a4381cbd90cf8adc45d4d4ebcae13
Arley HR <arley.hr@hotmail.com>
2019-06-16 18:03:44 -0500

    covers

---
+++ lib/apps/app_for_collectors/screens/console_detail.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+
+class ConsoleDetail extends StatelessWidget {
+  const ConsoleDetail({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+    );
+  }
+}
