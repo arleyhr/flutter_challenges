43fafa36972153dd60fb6055fead06cafa13a596
Arley HR <arley.hr@hotmail.com>
2019-06-16 20:41:11 -0500

    swiper games

--- lib/apps/app_for_collectors/screens/console_detail.dart
+++ lib/apps/app_for_collectors/screens/console_detail.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+
 import 'package:flutter_challenges/apps/app_for_collectors/models/console.dart';
 import 'package:flutter_challenges/apps/app_for_collectors/models/game.dart';
 
@@ -27,6 +29,7 @@ class _ConsoleDetailState extends State<ConsoleDetail> {
   @override
   Widget build(BuildContext context) {
     ConsoleDetailArguments arguments = ModalRoute.of(context).settings.arguments;
+    Size size = MediaQuery.of(context).size;
     Console console = arguments.console;
 
     List<Game> gamesList = games.where((game) => game.consoleId == console.id).toList();
@@ -41,12 +44,35 @@ class _ConsoleDetailState extends State<ConsoleDetail> {
         title: Text(_console.name),
       ),
       body: Container(
-        child: ListView.builder(
+        color: Color.fromRGBO(245, 246, 255, 1),
+        width: double.infinity,
+        height: double.infinity,
+        child: Swiper(
+          layout: SwiperLayout.STACK,
+          itemWidth: size.width * 0.6,
+          itemHeight: size.height * 0.45,
           itemCount: _games.length,
           itemBuilder: (BuildContext context, index) {
             final Game item = _games[index];
-            return ListTile(
-              title: Text(item.name),
+            print(item.image);
+            return Container(
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Theme.of(context).primaryColor.withAlpha(100),
+                    offset: Offset(0, 15),
+                    blurRadius: 20.0
+                  )
+                ]
+              ),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(20.0),
+                child: FadeInImage(
+                  fit: BoxFit.fill,
+                  placeholder: AssetImage('lib/apps/app_for_collectors/assets/placeholder.png'),
+                  image: NetworkImage(item.image, scale: 1.0),
+                ),
+              ),
             );
           },
         ),
