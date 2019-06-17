dbbd9e3c36e8936f8b51306d45fcfe7400522c19
Arley HR <arley.hr@hotmail.com>
2019-06-16 20:09:22 -0500

    passing console to detail

--- lib/apps/app_for_collectors/screens/console_detail.dart
+++ lib/apps/app_for_collectors/screens/console_detail.dart
@@ -1,12 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_challenges/apps/app_for_collectors/models/console.dart';
 
+class ConsoleDetailArguments {
+  ConsoleDetailArguments({ this.console });
 
-class ConsoleDetail extends StatelessWidget {
+  Console console;
+}
+
+class ConsoleDetail extends StatefulWidget {
   const ConsoleDetail({Key key}) : super(key: key);
 
   @override
+  _ConsoleDetailState createState() => _ConsoleDetailState();
+}
+
+class _ConsoleDetailState extends State<ConsoleDetail> {
+  Console _console;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container(
+    ConsoleDetailArguments arguments = ModalRoute.of(context).settings.arguments;
+
+    setState(() {
+      _console = arguments.console;
+    });
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_console.name),
+      ),
+      body: Container(),
     );
   }
 }
