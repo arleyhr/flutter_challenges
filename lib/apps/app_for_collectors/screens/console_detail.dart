643acc579ee9f7e14c437127b41fcf22975d802e
Arley HR <arley.hr@hotmail.com>
2019-06-20 00:31:25 -0500

    header detail refactor - sliver app bar

--- lib/apps/app_for_collectors/screens/console_detail.dart
+++ lib/apps/app_for_collectors/screens/console_detail.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_challenges/apps/app_for_collectors/widgets/bottom_bar.dart';
-import 'package:flutter_challenges/apps/app_for_collectors/widgets/games_swiper.dart';
 
 import 'package:flutter_challenges/apps/app_for_collectors/models/console.dart';
 import 'package:flutter_challenges/apps/app_for_collectors/models/game.dart';
+import 'package:flutter_challenges/apps/app_for_collectors/widgets/swiper_view.dart';
 
 class ConsoleDetailArguments {
   ConsoleDetailArguments({ this.console });
@@ -52,44 +52,20 @@ class _ConsoleDetailState extends State<ConsoleDetail> {
         _currentGame = game;
       });
     }
-
-    List<Widget> _SwipperView() {
-      return [
-        SwiperSection(currentGame: _currentGame, size: size, games: _games, onGameChange: _onGameChange),
-        Text(_currentGame.name, style: TextStyle(
-          color: Theme.of(context).primaryColor,
-          fontSize: 20.0
-        ), textAlign: TextAlign.center),
-        SizedBox(
-          height: 5,
-        ),
-        RatingStars(currentGame: _currentGame),
-        SizedBox(
-          height: 20,
-        ),
-        Container(
-          padding: EdgeInsets.symmetric(horizontal: 50),
-          child: Slider(
-            value: _currentGame.percent.toDouble(),
-            min: 0,
-            max: 100,
-            inactiveColor: Color.fromRGBO(200, 202, 221, 1),
-            onChanged: (value){
-              setState(() {
-                _currentGame.percent = value.toInt();
-              });
-            },
-          ),
-        )
-      ];
+    _onSliderChange(value) {
+      setState(() {
+        _currentGame.percent = value.toInt();
+      });
     }
 
     return Scaffold(
       bottomNavigationBar: BottomBar(color: Colors.white),
+      backgroundColor: Colors.white,
       body: CustomScrollView(
         slivers: <Widget>[
           SliverAppBar(
             elevation: 0,
+            floating: true,
             title: Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
@@ -111,13 +87,23 @@ class _ConsoleDetailState extends State<ConsoleDetail> {
                 icon: Icon(Icons.add, color: Colors.white, size: 30),
               )
             ],
+            expandedHeight: 180,
+            flexibleSpace: FlexibleSpaceBar(
+              centerTitle: false,
+              background: Container(
+                height: double.infinity,
+                alignment: Alignment.bottomLeft,
+                child: _buildHeaderConsole(context),
+              ),
+            ),
           ),
           if (!_isGridSelected)
-            SliverList(
-              delegate: SliverChildListDelegate([
-                _buildHeaderConsole(context),
-                  ..._SwipperView()
-              ])
+            SwiperView(
+              currentGame: _currentGame,
+              games: _games,
+              onGameChange: _onGameChange,
+              size: size,
+              onSliderChange: _onSliderChange,
             ),
           if (_isGridSelected)
             SliverGrid(
@@ -142,16 +128,18 @@ class _ConsoleDetailState extends State<ConsoleDetail> {
   Container _buildHeaderConsole(BuildContext context) {
     return Container(
       color: Theme.of(context).primaryColor,
+      height: 110,
       child: Stack(
         alignment: Alignment.center,
         children: <Widget>[
           Column(
             children: <Widget>[
               Container(
-                height: 35.0,
+                height: 40.0,
                 color: Theme.of(context).primaryColor,
               ),
               Container(
+                height: 70,
                 decoration: BoxDecoration(
                 color: Colors.white,
                   borderRadius: BorderRadius.only(
@@ -202,112 +190,6 @@ class _ConsoleDetailState extends State<ConsoleDetail> {
           color: _isGridSelected ? Theme.of(context).primaryColor : Colors.grey,
         ),
       ],
-    );
-  }
-}
-
-class RatingStars extends StatelessWidget {
-  const RatingStars({
-    Key key,
-    @required Game currentGame,
-  }) : _currentGame = currentGame, super(key: key);
-
-  final Game _currentGame;
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      mainAxisSize: MainAxisSize.min,
-      children: List.generate(5, (int index) {
-        return Icon(
-          index < _currentGame.rating ? Icons.star : Icons.star_border,
-          color: Color.fromRGBO(200, 202, 221, 1),
-          size: 22,
-        );
-      }),
-    );
-  }
-}
-
-class SwiperSection extends StatelessWidget {
-  const SwiperSection({
-    Key key,
-    @required Game currentGame,
-    @required this.size,
-    @required List<Game> games,
-    @required this.onGameChange,
-  }) : _currentGame = currentGame, _games = games, super(key: key);
-
-  final Game _currentGame;
-  final Size size;
-  final List<Game> _games;
-  final Function onGameChange;
-
-  @override
-  Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        RightStats(selectedGame: _currentGame),
-        GamesSwiper(size: size, games: _games, onItemChange: onGameChange),
-      ],
-    );
-  }
-}
-
-class RightStats extends StatelessWidget {
-  RightStats({
-    Key key,
-    this.selectedGame
-  }) : super(key: key);
-
-  final Game selectedGame;
-
-  @override
-  Widget build(BuildContext context) {
-    return Positioned(
-      top: 80.0,
-      right: 25.0,
-      child: Column(
-        children: <Widget>[
-          IconButton(
-            onPressed: (){},
-            icon: Icon(
-              Icons.bubble_chart, size: 35, color: Theme.of(context).primaryColor
-            ),
-          ),
-          Text('${selectedGame.percent}%', style: TextStyle(
-            fontSize: 18,
-            height: 0.5,
-            color: Colors.grey,
-            fontWeight: FontWeight.w100
-          )),
-          SizedBox(
-            height: 35,
-          ),
-          IconButton(
-            onPressed: (){},
-            icon: Icon(
-              Icons.people, size: 35, color: Theme.of(context).primaryColor
-            ),
-          ),
-          Text(selectedGame.people.toString(), style: TextStyle(
-            height: 0.5,
-            fontSize: 18,
-            color: Colors.grey,
-            fontWeight: FontWeight.w100
-          )),
-          SizedBox(
-            height: 35,
-          ),
-          IconButton(
-            onPressed: (){},
-            icon: Icon(
-              Icons.favorite_border, size: 35, color: Theme.of(context).primaryColor
-            ),
-          )
-        ],
-      ),
     );
   }
 }
---
+++ lib/apps/app_for_collectors/widgets/swiper_view.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_challenges/apps/app_for_collectors/models/game.dart';
+import 'package:flutter_challenges/apps/app_for_collectors/widgets/games_swiper.dart';
+import 'package:flutter_challenges/apps/app_for_collectors/widgets/stars_rating.dart';
+
+class SwiperView extends StatelessWidget {
+  SwiperView({
+    Key key,
+    this.currentGame,
+    this.size,
+    this.games,
+    this.onGameChange,
+    this.onSliderChange,
+  }) : super(key: key);
+
+  final Game currentGame;
+  final Size size;
+  final List<Game> games;
+  final Function onGameChange;
+  final Function onSliderChange;
+
+  @override
+  Widget build(BuildContext context) {
+    return SliverList(
+      delegate: SliverChildListDelegate([
+        Stack(
+          children: <Widget>[
+            RightStats(selectedGame: currentGame),
+            GamesSwiper(size: size, games: games, onItemChange: onGameChange),
+          ],
+        ),
+        Text(currentGame.name, style: TextStyle(
+          color: Theme.of(context).primaryColor,
+          fontSize: 20.0
+        ), textAlign: TextAlign.center),
+        SizedBox(
+          height: 5,
+        ),
+        RatingStars(value: currentGame.rating),
+        SizedBox(
+          height: 20,
+        ),
+        Container(
+          padding: EdgeInsets.symmetric(horizontal: 50),
+          child: Slider(
+            value: currentGame.percent.toDouble(),
+            min: 0,
+            max: 100,
+            inactiveColor: Color.fromRGBO(200, 202, 221, 1),
+            onChanged: onSliderChange,
+          ),
+        )
+      ])
+    );
+  }
+}
+
+
+class RightStats extends StatelessWidget {
+  RightStats({
+    Key key,
+    this.selectedGame
+  }) : super(key: key);
+
+  final Game selectedGame;
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      top: 80.0,
+      right: 25.0,
+      child: Column(
+        children: <Widget>[
+          IconButton(
+            onPressed: (){},
+            icon: Icon(
+              Icons.bubble_chart, size: 35, color: Theme.of(context).primaryColor
+            ),
+          ),
+          Text('${selectedGame.percent}%', style: TextStyle(
+            fontSize: 18,
+            height: 0.5,
+            color: Colors.grey,
+            fontWeight: FontWeight.w100
+          )),
+          SizedBox(
+            height: 35,
+          ),
+          IconButton(
+            onPressed: (){},
+            icon: Icon(
+              Icons.people, size: 35, color: Theme.of(context).primaryColor
+            ),
+          ),
+          Text(selectedGame.people.toString(), style: TextStyle(
+            height: 0.5,
+            fontSize: 18,
+            color: Colors.grey,
+            fontWeight: FontWeight.w100
+          )),
+          SizedBox(
+            height: 35,
+          ),
+          IconButton(
+            onPressed: (){},
+            icon: Icon(
+              Icons.favorite_border, size: 35, color: Theme.of(context).primaryColor
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
