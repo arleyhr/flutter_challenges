550bac223a95f156c2017633f6ceaa344c0d3460
Arley HR <arley.hr@hotmail.com>
2019-06-23 12:11:35 -0500

    refactor card button

--- lib/apps/app_for_collectors/main.dart
+++ lib/apps/app_for_collectors/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_challenges/apps/app_for_collectors/screens/console_detail.dart';
 import 'package:flutter_challenges/apps/app_for_collectors/screens/games_tab.dart';
 import 'package:flutter_challenges/apps/app_for_collectors/widgets/bottom_bar.dart';
+import 'package:flutter_challenges/apps/app_for_collectors/widgets/game_tabs_sliver_appbar.dart';
 
 Color primaryColor = Color.fromRGBO(63, 68, 157, 1);
 Color secondaryColor = Color.fromRGBO(128, 240, 180, 1);
@@ -60,85 +61,7 @@ class ScaffoldHome extends StatelessWidget {
     return Scaffold(
       body: CustomScrollView(
         slivers: <Widget>[
-          SliverAppBar(
-            leading: IconButton(
-              padding: EdgeInsets.all(0),
-              onPressed: () {},
-              icon: Icon(Icons.search, size: 30),
-            ),
-            title: Text('Your collection'),
-            centerTitle: true,
-            actions: <Widget>[
-              IconButton(
-                onPressed: () {},
-                icon: Icon(Icons.more_vert, size: 30),
-              )
-            ],
-            bottom: PreferredSize(
-              preferredSize: Size.fromHeight(70),
-              child: Container(
-                color: Color.fromRGBO(245, 246, 255, 1),
-                height: 70,
-                child: Stack(
-                  alignment: Alignment.topCenter,
-                  children: <Widget>[
-                    Positioned(
-                      bottom: 0,
-                      width: size.width * 0.75,
-                      child: Container(
-                        height: 30,
-                        decoration: BoxDecoration(
-                          color: Color.fromRGBO(197, 197, 227, 1),
-                          borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.circular(40),
-                            bottomRight: Radius.circular(40),
-                          )
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      width: size.width * 0.88,
-                      bottom: 8,
-                      child: Container(
-                        height: 50,
-                        decoration: BoxDecoration(
-                          color: Color.fromRGBO(147, 150, 199, 1),
-                          borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.circular(40),
-                            bottomRight: Radius.circular(40),
-                          )
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      child: Container(
-                        height: 50,
-                        decoration: BoxDecoration(
-                          color: primaryColor,
-                          borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.circular(40),
-                            bottomRight: Radius.circular(40),
-                          )
-                        ),
-                      ),
-                    ),
-                    Container(
-                      width: size.width * 0.65,
-                      child: TabBar(
-                        controller: _tabController,
-                        indicatorWeight: 4,
-                        tabs: <Widget>[
-                          Tab(text: 'Games'),
-                          Tab(text: 'Books'),
-                          Tab(text: 'Movies'),
-                        ],
-                      )
-                    )
-                  ],
-                ),
-              ),
-            ),
-          ),
+          GameTabsSliverAppBar(size: size, tabController: _tabController),
           SliverFillRemaining(
             child: TabBarView(
               controller: _tabController,
@@ -155,3 +78,4 @@ class ScaffoldHome extends StatelessWidget {
     );
   }
 }
+
