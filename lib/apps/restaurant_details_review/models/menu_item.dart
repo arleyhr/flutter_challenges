dc70ef1286889e3f21b7a595b086aff12f780450
Arley HR <arley.hr@hotmail.com>
2019-07-09 18:29:50 -0500

    menu item model

---
+++ lib/apps/restaurant_details_review/models/menu_item.dart
@@ -0,0 +1,19 @@
+class MenuItem {
+  int id;
+  String name;
+  String image;
+  MenuItem({ this.id, this.name, this.image });
+}
+
+final imagePrefix = 'https://github.com/arleyhr/flutter_challenges/blob/develop/lib/apps/restaurant_details_review/assets/';
+
+List<MenuItem> menuItems = [
+  MenuItem(id: 1, name: 'Fries', image: '${imagePrefix}fries.png?raw=true'),
+  MenuItem(id: 2, name: 'Sushi', image: '${imagePrefix}sushi.png?raw=true'),
+  MenuItem(id: 3, name: 'Pizza', image: '${imagePrefix}pizza.png?raw=true'),
+  MenuItem(id: 4, name: 'Noodles', image: '${imagePrefix}noddles.png?raw=true'),
+  MenuItem(id: 5, name: 'Burger', image: '${imagePrefix}burger.png?raw=true'),
+  MenuItem(id: 6, name: 'Cupcake', image: '${imagePrefix}cupcake.png?raw=true'),
+  MenuItem(id: 7, name: 'Eggs', image: '${imagePrefix}eggs.png?raw=true'),
+  MenuItem(id: 8, name: 'Melon', image: '${imagePrefix}melon.png?raw=true')
+];
