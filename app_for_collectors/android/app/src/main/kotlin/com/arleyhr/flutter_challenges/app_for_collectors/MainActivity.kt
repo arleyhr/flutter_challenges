5a5d5dc2189aee7a3758aa7390972ceeb2396d57
Arley HR <arley.hr@hotmail.com>
2019-12-15 13:06:53 -0500

    modularize apps

---
+++ app_for_collectors/android/app/src/main/kotlin/com/arleyhr/flutter_challenges/app_for_collectors/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.arleyhr.flutter_challenges.app_for_collectors
+
+import androidx.annotation.NonNull;
+import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+    override fun configureFlutterEngine(@NonNull flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+}
---
+++ main/lib/home.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:restaurant_details_review/routes.dart';
+import 'package:shared/application.dart';
+
+import 'package:app_for_collectors/routes.dart';
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key key}) : super(key: key);
+
+  _goToPage (context, page) {
+    Application.router.navigateTo(context, page);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Challenges',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Flutter Challenges'),
+        ),
+        body: Container(
+          child: ListView(
+            children: <Widget>[
+              FlatButton(
+                onPressed: () => _goToPage(context, AppForCollectorsRoutes.homePath),
+                child: Text('App For Collectors'),
+              ),
+              FlatButton(
+                onPressed: () => _goToPage(context, RestaurantDetailsReviewRoutes.homePath),
+                child: Text('Restaurants Details Review'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
